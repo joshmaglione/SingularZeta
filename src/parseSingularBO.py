338250ba7f65898222061dbba2c9a5f7f36406a7
Josh Maglione <joshmaglione@gmail.com>
2019-03-20 21:33:59 +0100

    Minor edits

--- src/parseSingularBO.py
+++ src/parseSingularBO.py
@@ -4,6 +4,15 @@
 #   Distributed under MIT License
 #
 
+
+def _get_row_num(factors):
+    row = 0 
+    for fact in factors:
+        if "gen(" in fact[0]:
+            row = int(fact[0].replace("gen(", "").replace(")", ""))
+    return row        
+
+
 # Given a Singular term as a string (probably from _expr_to_terms), return the 
 # tuple of pairs of type (str, int). Each entry corresponds to a factor of the 
 # tuple, and the integer its corresponding power.
@@ -23,18 +32,21 @@ def _term_to_factors(term):
 # Given a Singular expression as a string, returns the tuple of strings 
 # corresponding to the terms of the expression given.
 def _expr_to_terms(exp):
+    # Treat negative terms as -1 * the term.
     # This will quickly get us a coarse splitting up.
-    some_terms = exp.split("+")
+    some_terms = exp.replace("-", "+-1*").split("+")
     terms = []
     # Now we run through all the terms from above looking for negative terms 
     # that we missed.
     for term in some_terms:
-        while "-" in term[1:]:
-            ind = term.index("-")
-            if ind > 0:
-                terms += [term[:ind]]
-            # Keep the negative, so start at ind.
-            terms = term[ind:] 
         if len(term) > 0:
             terms += [term]
     return tuple(terms)
+
+
+# COMEBACK HERE
+def _expr_to_tup(exp):
+    terms = _expr_to_terms(exp)
+    fact_terms = [_term_to_factors(term) for term in terms]
+    n = max(_get_row_num(fact) for fact in fact_terms)
+    return n
