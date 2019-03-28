bf0722095bdf8d76e62bdfd4fe601ed85038648c
joshmaglione <joshmaglione@gmail.com>
2019-03-28 16:37:47 +0100

    Cleaned up '/__init__.py'

--- __init__.py
+++ __init__.py
@@ -10,19 +10,20 @@ __version__ = 0.1
 print("Loading...")
 
 # === This is very annoying during development =================================
-import sys, os
-sys.dont_write_bytecode = True
+import sys as _sys
+_sys.dont_write_bytecode = True
 # ==============================================================================
 
 # Enables us to turn off printing.
+from os import devnull as _DEVNULL
 class _HiddenPrints:
     def __enter__(self):
-        self._original_stdout = sys.stdout
-        sys.stdout = open(os.devnull, 'w')
+        self._original_stdout = _sys.stdout
+        _sys.stdout = open(_DEVNULL, 'w')
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        sys.stdout.close()
-        sys.stdout = self._original_stdout
+        _sys.stdout.close()
+        _sys.stdout = self._original_stdout
 
 
 # Start a Singular run
@@ -37,7 +38,8 @@ except NameError:
     try: 
         # This just turns off printing because the Zeta banner always comes up.
         with _HiddenPrints():
-            import Zeta
+            # TODO: EVENTUALLY SPECIFY.
+            import Zeta 
         Zeta_ver = isinstance(Zeta.__version__, str)
     except ImportError:
         Zeta_ver = False
@@ -53,15 +55,13 @@ if Zeta_ver:
 else:
     print "    Could not find Zeta! Most functions unavailable."
     print "    Zeta url: http://www.maths.nuigalway.ie/~rossmann/Zeta/"
-
+del Zeta_ver
 
 # 'from foo import *' leaves hidden functions hidden and brings it up to 
 # TensorSpace instead of TensorSpace.src
 # Load interface
 from src.interfaceSing import *
 from src.ringClass import *
-from src.parseSingularBasics import _parse_list as parseList #REMOVE
-from src.parseSingularExpr import _expr_to_terms as exprToTerms #REMOVE
 
 
 # Sage is still on python2.
