125ee534c393c158dfa5ee97a5d97ab847759102
joshmaglione <joshmaglione@gmail.com>
2019-08-27 14:47:31 +0200

    Turned dev printing on

--- src/globalVars.py
+++ src/globalVars.py
@@ -15,7 +15,7 @@ _DEFAULT_LOAD_DB = True         # Boolean
 _DEFAULT_p = 'p'                # String
 _DEFAULT_t = 't'                # String
 _DEFAULT_USER_INPUT = True      # Boolean
-_DEFAULT_VERBOSE = 0            # Integer
+_DEFAULT_VERBOSE = 2            # Integer
 
 
 # Variables for Singular things
